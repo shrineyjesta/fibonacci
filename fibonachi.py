45e7c464897918be1da568782506d289d9b515be
shrineyjesta <97728736+shrineyjesta@users.noreply.github.com>
2022-01-14 14:27:56 +0530

    Add files via upload

---
+++ fibonachi.py
@@ -0,0 +1,12 @@
+num=int(input("enter a num"))
+a=0
+b=1
+sum=0
+while(num<0):
+    print("print greater than 0")
+else:
+    for i in range(0,num):
+        print(sum)
+        a=b
+        b=sum
+        sum = a+b
